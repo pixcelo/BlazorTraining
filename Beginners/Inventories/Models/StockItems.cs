a18ffc8c92101cb7c81e5cd6fa662cf54462d3bb
tetsu.k <bodomu@gmail.com>
2024-10-14 06:35:36 +0900

    [add] 在庫品のモデルクラス

---
+++ Beginners/Inventories/Models/StockItems.cs
@@ -0,0 +1,18 @@
+﻿namespace Inventories.Models
+{
+    /// <summary>
+    /// 在庫品
+    /// </summary>
+    public sealed class StockItems
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; } = "";
+
+        public string? LotNumber { get; set; } = "";
+
+        public int Quantity { get; set; }
+
+        public DateTime ArrivalDate { get; set; }
+    }
+}
